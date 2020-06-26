cda3b5ac7389381bb07833223de1d0ec4d7a0244
ORN-git <Y5$BqK^'D/>
2020-06-26 12:17:38 -0700

    Moved the plot of Critical vs time outside the loop by using df_C_vs_T. Add disease called 'deterministic_test' for testing.

--- MonteCarloSEIR.py
+++ MonteCarloSEIR.py
@@ -36,10 +36,15 @@ def Model(initial_cases, initial_date, N, p_immune, beta, gamma, delta, p_I_to_C
 N = 100000  # size of initial population
 initial_cases = 10  # initial number of Exposed. I chose 10 to stay away from the critical community size at the onset of the epidemic.
 
-num_reps = 1000  # for Monte Carlo
+num_reps = 10  # for Monte Carlo
 
-disease = 'Influenza_seasonal'  # supported options: 'Covid_19', 'Influenza_seasonal'
+disease = 'Covid_19'  # supported options: 'Covid_19', 'Influenza_seasonal', or 'deterministic_test'
 print(disease)
+# times below are assumed to be common for Covid-19 and influenza seasonal
+# todo: if supporting more disease, and they don't share the same timing, move those into the "if disease " above.
+T_I_to_C = np.random.uniform(4.5, 12.5, num_reps).round(2)  # time to transition from Infected to Critical [days]
+T_C_to_D = np.random.uniform(5, 9, num_reps).round(2)  # time to transition from Critical to Dead [days]
+T_C_to_R = np.random.uniform(12, 16, num_reps).round(2)  # time to transition from Critical to Recovered [days]
 
 # The section below is intended to be modular, so we can easily add other disease model.
 if disease == 'Covid_19':
@@ -62,78 +67,87 @@ elif disease == 'Influenza_seasonal':
     beta = R_0 / T_infectious
     p_I_to_C = np.random.uniform(0.25/100, 0.75/100, num_reps).round(3) # overall probability of transition from Infected to Critical
     p_C_to_D = np.random.uniform(33/100, 48/100, num_reps).round(3) # overall probability of transition from Infected to Critical
+elif disease == 'deterministic_test':
+    R_0 = np.random.uniform(3, 3, num_reps).round(2)  # Initial R0
+    p_immune = np.random.uniform(0/100, 0/100, num_reps).round(3)  # portion of immuned population
+    T_infectious = np.random.uniform(9, 9, num_reps).round(2)  # infectious period [days]
+    T_incubation = np.random.uniform(3, 3, num_reps).round(2)  # incubation period [days] - not contagious during this time
+    gamma = 1.0 / T_infectious
+    delta = 1.0 / T_incubation
+    beta = R_0 / T_infectious
+    p_I_to_C = np.random.uniform(5/100, 5/100, num_reps).round(3) # overall probability of transition from Infected to Critical
+    p_C_to_D = np.random.uniform(30/100, 30/100, num_reps).round(3) # overall probability of transition from Infected to Critical
+    T_I_to_C = np.random.uniform(12, 12, num_reps).round(2)  # time to transition from Infected to Critical [days]
+    T_C_to_D = np.random.uniform(7.5, 7.5, num_reps).round(2)  # time to transition from Critical to Dead [days]
+    T_C_to_R = np.random.uniform(6.5, 6.5, num_reps).round(2)  # time to transition from Critical to Recovered [days]
 else:
     print('undefined disease!')
 
 
-# times below are assumed to be common for Covid-19 and influenza seasonal
-# todo: if supporting more disease, and they don't share the same timing, move those into the "if disease " above.
-T_I_to_C = np.random.uniform(4.5, 12.5, num_reps).round(2) # time to transition from Infected to Critical [days]
-T_C_to_D = np.random.uniform(5, 9, num_reps).round(2) # time to transition from Critical to Dead [days]
-T_C_to_R = np.random.uniform(12, 16, num_reps).round(2) # time to transition from Critical to Recovered [days]
+
 initial_date = 0
 
 ############### Monte Carlo ###############
 print('start Monte-Carlo')
-results_max_C = []
-results_max_D = []
-results_tot_CFR = []
+# results_max_C = []
+# results_max_D = []
+# results_tot_CFR = []
+df_results = pd.DataFrame({})  # initiate as an empty DataFrame so I can append to it in the Monte Carlo loop
+df_C_vs_t = pd.DataFrame([])  # initiate as an empty DataFrame so I can append to it in the Monte Carlo loop
+C_vs_t = []
 for i in range(num_reps):
     time, S, E, I, C, R, D, total_CFR, daily_CFR = Model(initial_cases, initial_date, N, p_immune[i], beta[i], gamma[i], delta[i],  p_I_to_C[i], T_I_to_C[i], p_C_to_D[i], T_C_to_R[i], T_C_to_D[i])
-    results_max_C.append(max(C))
-    results_max_D.append(max(D))
-    results_tot_CFR.append(max(total_CFR))
+    # results_max_C.append(max(C))
+    # results_max_D.append(max(D))
+    # results_tot_CFR.append(max(total_CFR))
+    df_temp = pd.DataFrame(index=[i], data={'R_0': R_0[i], 'immune %': p_immune[i] * 100,
+                                       'T_infectious': T_infectious[i], 'T_incubation': T_incubation[i],
+                                       'p_I_to_C%': p_I_to_C[i] * 100, 'p_C_to_D%': p_C_to_D[i] * 100,
+                                       'T_I_to_C': T_I_to_C[i], 'T_C_to_R': T_C_to_R[i], 'T_C_to_D': T_C_to_D[i],
+                                       'peak E': max(E), 'peak I': max(I), 'peak R': max(R),
+                                       'peak C': max(C), 'max D': max(D), 'total IFR': max(total_CFR)})
+    df_results = df_results.append(df_temp)
+    #
+    # df_temp_compartment = pd.DataFrame(index=[i], data={'C': C})
 
-    # todo: have to pull the plotting outside of the loop.
-    #  Save results vectors and plot as one, outside the Monte Carlo loop.
-    #  When doing that, plot peak Critical, total Dead and CFR (IFR) over time
-    # plot Critical as a function of time
-    plt.plot(time, C)
-    plt.title('Critical compartment vs time, for all Monte Carlo runs')
-    plt.xlabel('time [days]')
-    plt.ylabel('Critical')
-    plt.show()
-    # # plot CFR as a function of time
-    # plt.plot(time, total_CFR)
-    # plt.title('total_CFR vs time, for all Monte Carlo runs')
-    # plt.xlabel('time [days]')
-    # plt.ylabel('total_CFR')
-    # plt.show()
+    C_vs_t.append(C)  # thanks to https://stackoverflow.com/questions/31674557/how-to-append-rows-in-a-pandas-dataframe-in-a-for-loop for this method
+    df_C_vs_t = pd.DataFrame(C_vs_t)  # compartment value for each day is appended as new row
 
-####################### collect data ##########################
-print('re-arranging data')
-df = pd.DataFrame(index=range(num_reps), data={'R_0': R_0, 'immune %': p_immune * 100,
-                                               'T_infectious': T_infectious, 'T_incubation': T_incubation,
-                                               'p_I_to_C%': p_I_to_C * 100, 'p_C_to_D%': p_C_to_D * 100,
-                                               'T_I_to_C': T_I_to_C, 'T_C_to_R': T_C_to_R, 'T_C_to_D': T_C_to_D,
-                                               'peak C': results_max_C, 'peak D': results_max_D,
-                                               'total_CFR': results_tot_CFR})
+
 # Set it to None to display all columns in the dataframe,
 # but actually print to terminal only if <30 rows (30 being arbitrary, used for testing)
 if num_reps < 30:
     pd.set_option('display.max_columns', None)
-    print(df)
+    print(df_results)
 else:
-    print('NOT printing df as it is deemed too long')
-
+    print('NOT printing results as it is deemed too long')
+print('min / max I')
+print(min(df_results['peak I']), max(df_results['peak I']))
 print('min / max C')
-print(min(results_max_C), max(results_max_C))
+print(min(df_results['peak C']), max(df_results['peak C']))
 print('min / max D')
-print(min(results_max_D), max(results_max_D))
-print('min(CFR), max(CFR)')
-print(min(results_tot_CFR), max(results_tot_CFR))
+print(min(df_results['max D']), max(df_results['max D']))
+print('min(IFR), max(IFR)')
+print('min / max R')
+print(min(df_results['peak R']), max(df_results['peak R']))
+print(min(df_results['total IFR']), max(df_results['total IFR']))
 
 ####################### plot ##########################
 
-print('now plot histogram')
+print('plot compartment vs time')
 
-df.hist(column='peak C')
+df_C_vs_t.T.plot()  # Transpose as the data is in the rows
 plt.show()
 
-df.hist(column='peak D')
+print('now plot histograms')
+
+df_results.hist(column='peak C')
 plt.show()
 
-df.hist(column='total_CFR')
+df_results.hist(column='max D')
+plt.show()
+
+df_results.hist(column='total IFR')
 plt.show()
 
 
